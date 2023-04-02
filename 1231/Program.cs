e31116413751bf2a45f0d710c0eace7fddfaa6ab
Kleo <mmttdd16@gmail.com>
2023-04-02 14:14:04 +0300

    new file:   1231 new file:   Ex1v2

---
+++ 1231/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        while (true) {
+            Console.Write("Введите число: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number % 2 == 0) {
+                Console.WriteLine("Число четное");
+            }
+            else {
+                Console.WriteLine("Число нечетное");
+                continue; // возвращаемся к началу цикла
+            }
+        }
+    }
+}
--- Ex1/Program.cs
+++ Ex1/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+
+class Program {
+    static void Main(string[] args) {
+        while (true) {
+            Console.Write("Введите число трехзначное числое: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number >= 100 && number < 1000) {
+                int secondNumber = (number / 10) % 10;
+                System.Console.WriteLine(secondNumber);
+                break;
+            }
+            else {
+                Console.WriteLine("Ошибка");
+                continue; // возвращаемся к началу цикла
+            }
+        }
+    }
+}
+
