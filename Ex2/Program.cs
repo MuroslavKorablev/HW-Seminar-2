f83987effef2fbeb77d3eb52d0886fd0b3e36bd4
Kleo <mmttdd16@gmail.com>
2023-06-02 22:45:00 +0300

    Finished ex2

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,22 +1,52 @@
-﻿ //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
- 
- using System;
+﻿using System;
 
-class Program {
-    static void Main(string[] args) {
-        while (true) {
-            Console.Write("Введите число трехзначное числое: ");
-            int number = int.Parse(Console.ReadLine());
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите целое число: ");
+        int number = int.Parse(Console.ReadLine());
 
-            if (number >= 100 && number < 1000) {
-                int thirdNumber = number % 10;  
-                System.Console.WriteLine(thirdNumber);
-                break;
-            }
-            else {
-                Console.WriteLine("Ошибка");
-                continue; // возвращаемся к началу цикла
-            }
+        int thirdDigit = GetThirdDigit(number);
+
+        if (thirdDigit != -1)
+        {
+            Console.WriteLine($"Третья цифра: {thirdDigit}");
+        }
+        else
+        {
+            Console.WriteLine("Третьей цифры нет");
         }
     }
+
+    static int GetThirdDigit(int number)
+    {
+        int count = CountDigits(number);
+
+        if (count < 3)
+        {
+            return -1; // Третьей цифры нет
+        }
+
+        int thirdDigit = 0;
+        for (int i = 0; i < count - 3; i++)
+        {
+            number /= 10;
+        }
+        thirdDigit = number % 10;
+
+        return thirdDigit;
+    }
+
+    static int CountDigits(int number)
+    {
+        int count = 0;
+        while (number != 0)
+        {
+            number /= 10;
+            count++;
+        }
+        return count;
+    }
 }
+
