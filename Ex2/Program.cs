3072cbd8bb9aa9d5b522774fac77136f52ded8c3
Kleo <mmttdd16@gmail.com>
2023-06-02 22:36:37 +0300

    Qe

--- Ex2/Program.cs
+++ Ex2/Program.cs
@@ -1,2 +1,22 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿ //Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+ 
+ using System;
+
+class Program {
+    static void Main(string[] args) {
+        while (true) {
+            Console.Write("Введите число трехзначное числое: ");
+            int number = int.Parse(Console.ReadLine());
+
+            if (number >= 100 && number < 1000) {
+                int thirdNumber = number % 10;  
+                System.Console.WriteLine(thirdNumber);
+                break;
+            }
+            else {
+                Console.WriteLine("Ошибка");
+                continue; // возвращаемся к началу цикла
+            }
+        }
+    }
+}
---
+++ Ex3/Program.cs
@@ -0,0 +1,15 @@
+﻿System.Console.WriteLine("Введите день недели");
+int dayOfWeek = Convert.ToInt32(Console.ReadLine());
+if (dayOfWeek == 6 || dayOfWeek == 7)
+{
+    System.Console.WriteLine("Это выходной");
+}
+else if (dayOfWeek > 7 || dayOfWeek < 1 )
+{
+    System.Console.WriteLine("Error");
+}
+else
+{
+    System.Console.WriteLine("Это не выходной");
+}
+
